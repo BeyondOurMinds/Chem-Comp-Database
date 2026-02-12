532a88e1399b2401b8b1f9ef33666f4396f67165
John Hardin <84035484+BeyondOurMinds@users.noreply.github.com>
2026-02-12 01:09:41 +0000

    Grabbing required entries for database

--- src/Chem_Database/database.py
+++ src/Chem_Database/database.py
@@ -1,4 +1,6 @@
 from rdkit.Chem import PandasTools
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
 import sqlite3
 
 df = PandasTools.LoadSDF("data/Molecules11.sdf", smilesName="SMILES")
@@ -12,9 +14,20 @@ if 'ROMol' in df.columns:
     df = df.drop('ROMol', axis=1)  # Drop the non-serializable ROMol column
 
 # Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('data/chem_database.db')
-c = conn.cursor()
-df.to_sql(name='molecules', con=conn, if_exists='replace', index=False)
-conn.commit()
-conn.close()
+con = sqlite3.connect('data/chem_database.db')
+cur = con.cursor()
+for row in df.itertuples():
+    id = row.CdId
+    smiles = row.SMILES
+    mol = Chem.MolFromSmiles(smiles)
+    bond = rdMolDescriptors.CalcNumRotatableBonds(mol)
+    logp = rdMolDescriptors.CalcCrippenDescriptors(mol)[0]
+    bonddonor = rdMolDescriptors.CalcNumHBD(mol)
+    bondacceptor = rdMolDescriptors.CalcNumHBA(mol)
+    ringCount = rdMolDescriptors.CalcNumRings(mol)
+    molWeight = rdMolDescriptors.CalcExactMolWt(mol)
+
+#df.to_sql(name='molecules', con=con, if_exists='replace', index=False)
+con.commit()
+con.close()
 print(f"\nDatabase created and {len(df)} rows inserted successfully.")
